761740c481e97c36b07377bb149c9bba97d4a67c
Juan Ezquerro LLanes <arrase@gmail.com>
2018-06-24 02:09:58 +0200

    [2/2] OmniBrain : Adds do not disturb runner
    Change-Id: I58352c716873ef60312a918ed8c8552d87ace652

---
+++ src/org/omnirom/omnibrain/runners/DNDRunner.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (C) 2018 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.omnirom.omnibrain.runners;
+
+import android.app.NotificationManager;
+import android.content.Context;
+
+public class DNDRunner {
+    private NotificationManager mNoMan;
+
+    void DNDRunner(Context context) {
+        mNoMan = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    public void run(String param) {
+        mNoMan.setZenMode(Integer.parseInt(param), null, "DNDRunner");
+    }
+}
