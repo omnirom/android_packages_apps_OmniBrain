af4b520aff08bc6ef0fb6f4852dbe9035d19bc3f
Juan Ezquerro LLanes <arrase@gmail.com>
2018-08-07 20:54:26 +0200

    [WIP][1/2] OmniBrain : Changes for OmniBrain v2.0
    Available events:
    
     * Media Player events
     * Wifi Events
     * Lifestyle Events
     * Bluetooth Events
     * NFC Events
    
    Lifestyle events are those that occur in a certain
    time slot and place, for example waiting for the
    bus to go to work in the mornings.
    
    Change-Id: I9ae0eb9d26e8cb69683e212c60018182a98b4c90

--- src/org/omnirom/omnibrain/fragments/PublicNetworkEventsSettings.java
+++ src/org/omnirom/omnibrain/fragments/PublicNetworkEventsSettings.java
@@ -16,7 +16,7 @@
  *
  */
 
-package org.omnirom.omnibrain;
+package org.omnirom.omnibrain.fragments;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -24,12 +24,9 @@ import android.os.Bundle;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.text.TextUtils;
-import android.support.v14.preference.PreferenceFragment;
 
 import org.omnirom.omnibrain.R;
-
-import org.omnirom.omnilib.preference.OmniActionsListPreference;
-import org.omnirom.omnilib.preference.WifiSelectListPreference;
+import org.omnirom.omnibrain.actions.ActionListPreference;
 import org.omnirom.omnilib.fragments.OmniLibPreferenceFragment;
 
 import java.util.ArrayList;
@@ -38,16 +35,15 @@ import java.util.Collection;
 import java.util.List;
 
 public class PublicNetworkEventsSettings extends OmniLibPreferenceFragment implements OnPreferenceChangeListener {
-    public static final String EVENTS_PREFERENCES_NAME = "event_service";
 
     public static final String PUBLIC_CONNECT_ACTIONS = "public_connect_actions";
     public static final String PUBLIC_DISCONNECT_ACTIONS = "public_disconnect_actions";
 
-    private OmniActionsListPreference mPublicConnectActions;
-    private OmniActionsListPreference mPublicDisconnectActions;
+    private ActionListPreference mPublicConnectActions;
+    private ActionListPreference mPublicDisconnectActions;
 
     private SharedPreferences getPrefs() {
-        return getActivity().getSharedPreferences(EVENTS_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        return getActivity().getSharedPreferences(EventCategoryFragment.EVENTS_PREFERENCES_NAME, Context.MODE_PRIVATE);
     }
 
     @Override
@@ -55,21 +51,20 @@ public class PublicNetworkEventsSettings extends OmniLibPreferenceFragment imple
         addPreferencesFromResource(R.xml.public_network_settings);
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mPublicConnectActions = (OmniActionsListPreference) findPreference(PUBLIC_CONNECT_ACTIONS);
+        mPublicConnectActions = (ActionListPreference) findPreference(PUBLIC_CONNECT_ACTIONS);
         mPublicConnectActions.loadActions(R.xml.omni_actions);
         String value = getPrefs().getString(PUBLIC_CONNECT_ACTIONS, null);
         List<String> valueList = new ArrayList<String>();
         if (!TextUtils.isEmpty(value)) {
             valueList.addAll(Arrays.asList(value.split(":")));
         }
-        mPublicConnectActions.setValues(valueList);
         mPublicConnectActions.setSummary(String.format(
                 getResources().getString(R.string.omni_actions_summary),
                 valueList.size()
         ));
         mPublicConnectActions.setOnPreferenceChangeListener(this);
 
-        mPublicDisconnectActions = (OmniActionsListPreference) findPreference(PUBLIC_DISCONNECT_ACTIONS);
+        mPublicDisconnectActions = (ActionListPreference) findPreference(PUBLIC_DISCONNECT_ACTIONS);
         mPublicDisconnectActions.loadActions(R.xml.omni_actions);
         value = getPrefs().getString(PUBLIC_DISCONNECT_ACTIONS, null);
         valueList = new ArrayList<String>();
