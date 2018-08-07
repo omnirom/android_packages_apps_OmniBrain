a9218aeaf8d075be79ec827d051187bfa8026c29
Juan Ezquerro LLanes <arrase@gmail.com>
2018-08-07 20:54:26 +0200

    [WIP][1/2] OmniBrain : Changes for OmniBrain v2.0
    Available events:
    
     * Media Player events
     * Wifi Events
     * Lifestyle Events
     * Bluetooth Events
     * NFC Events
    
    Lifestyle events are those that occur in a certain
    time slot and place, for example waiting for the
    bus to go to work in the mornings.
    
    Change-Id: I9ae0eb9d26e8cb69683e212c60018182a98b4c90

---
+++ src/org/omnirom/omnibrain/fragments/WifiEventsSettings.java
@@ -0,0 +1,81 @@
+/*
+ *  Copyright (C) 2018 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.omnirom.omnibrain.fragments;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+
+import org.omnirom.omnilib.fragments.OmniLibPreferenceFragment;
+import org.omnirom.omnilib.preference.SeekBarPreference;
+
+public class WifiEventsSettings extends OmniLibPreferenceFragment implements OnPreferenceChangeListener {
+
+    public static final String DISABLE_WIFI_THRESHOLD = "disable_wifi_threshold";
+    public static final String HOME_WIFI_PREFERENCE_SCREEN = "home_network_events";
+    public static final String WORK_WIFI_PREFERENCE_SCREEN = "work_network_events";
+
+    private SeekBarPreference mDisableWifi;
+    private Preference homeWifi;
+    private Preference workWifi;
+
+    private SharedPreferences getPrefs() {
+        return getActivity().getSharedPreferences(EventCategoryFragment.EVENTS_PREFERENCES_NAME, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.wifi_events_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        boolean locationDisabled = Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.LOCATION_MODE, -1) == 0;
+
+        mDisableWifi = (SeekBarPreference) findPreference(DISABLE_WIFI_THRESHOLD);
+        mDisableWifi.setValue(getPrefs().getInt(DISABLE_WIFI_THRESHOLD, 0));
+        mDisableWifi.setOnPreferenceChangeListener(this);
+        mDisableWifi.setEnabled(!locationDisabled);
+
+        homeWifi = findPreference(HOME_WIFI_PREFERENCE_SCREEN);
+        homeWifi.setEnabled(!locationDisabled);
+
+        workWifi = findPreference(WORK_WIFI_PREFERENCE_SCREEN);
+        workWifi.setEnabled(!locationDisabled);
+
+        if (locationDisabled) {
+            mDisableWifi.setSummary(R.string.wifi_location_disabled);
+            homeWifi.setSummary(R.string.wifi_location_disabled);
+            workWifi.setSummary(R.string.wifi_location_disabled);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mDisableWifi) {
+            int value = ((int) newValue);
+            getPrefs().edit().putInt(DISABLE_WIFI_THRESHOLD, value).commit();
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/org/omnirom/omnibrain/service/EventService.java
@@ -0,0 +1,148 @@
+/*
+ *  Copyright (C) 2018 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.omnirom.omnibrain.service;
+
+import android.app.Service;
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.media.AudioManager;
+import android.net.wifi.WifiManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.util.Log;
+
+import org.omnirom.omnibrain.events.MediaPlayerEvents;
+import org.omnirom.omnibrain.events.WifiEvents;
+
+
+public class EventService extends Service {
+    public static final String EVENTS_PREFERENCES_NAME = "event_service";
+
+    private static final String TAG = "OmniEventService";
+    private static final boolean DEBUG = false;
+    private static boolean mIsRunning;
+    private final LocalBinder mBinder = new LocalBinder();
+    private PowerManager.WakeLock mWakeLock;
+    private int mOverlayWidth;
+
+    private BroadcastReceiver mStateListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            mWakeLock.acquire();
+
+            try {
+                if (DEBUG) Log.d(TAG, "onReceive " + action);
+                switch (action) {
+                    case BluetoothAdapter.ACTION_STATE_CHANGED:
+                        // TODO
+                        break;
+                    case BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED:
+                        MediaPlayerEvents.runEvent(context, getPrefs(context), "a2dp", mOverlayWidth);
+                        break;
+                    case AudioManager.ACTION_HEADSET_PLUG:
+                        MediaPlayerEvents.runEvent(context, getPrefs(context), "headset", mOverlayWidth);
+                        break;
+                    case WifiManager.NETWORK_STATE_CHANGED_ACTION:
+                        WifiEvents.runEvent(context, getPrefs(context));
+                        break;
+                }
+            } finally {
+                mWakeLock.release();
+            }
+        }
+    };
+
+    public static boolean isRunning() {
+        return mIsRunning;
+    }
+
+    private SharedPreferences getPrefs(Context context) {
+        return context.getSharedPreferences(EVENTS_PREFERENCES_NAME, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (DEBUG) Log.d(TAG, "onCreate");
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+        mWakeLock.setReferenceCounted(true);
+        mIsRunning = true;
+        registerListener();
+        mOverlayWidth = getOverlayWidth(this);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mOverlayWidth = getOverlayWidth(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (DEBUG) Log.d(TAG, "onDestroy");
+        unregisterListener();
+        mIsRunning = false;
+    }
+
+    private void registerListener() {
+        if (DEBUG) Log.d(TAG, "registerListener");
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED);
+        filter.addAction(AudioManager.ACTION_HEADSET_PLUG);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        this.registerReceiver(mStateListener, filter);
+    }
+
+    private void unregisterListener() {
+        if (DEBUG) Log.d(TAG, "unregisterListener");
+        try {
+            this.unregisterReceiver(mStateListener);
+        } catch (Exception e) {
+            Log.e(TAG, "unregisterListener", e);
+        }
+    }
+
+    private int getOverlayWidth(Context context) {
+        return (context.getResources().getDimensionPixelSize(R.dimen.floating_widget_view_padding) +
+                context.getResources().getDimensionPixelSize(android.R.dimen.app_icon_size)) / 2;
+    }
+
+    public class LocalBinder extends Binder {
+        public EventService getService() {
+            return EventService.this;
+        }
+    }
+
+}
+
