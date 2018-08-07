83d65a9e89890a3a37126d31d59eb6e5e6d32486
Juan Ezquerro LLanes <arrase@gmail.com>
2018-08-07 20:54:26 +0200

    [WIP][1/2] OmniBrain : Changes for OmniBrain v2.0
    Available events:
    
     * Media Player events
     * Wifi Events
     * Lifestyle Events
     * Bluetooth Events
     * NFC Events
    
    Lifestyle events are those that occur in a certain
    time slot and place, for example waiting for the
    bus to go to work in the mornings.
    
    Change-Id: I9ae0eb9d26e8cb69683e212c60018182a98b4c90

---
+++ src/org/omnirom/omnibrain/ui/SimpleDividerItemDecoration.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2018 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.omnirom.omnibrain.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.omnirom.omnilib.R;
+
+
+public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
+    private final Drawable mDivider;
+
+    public SimpleDividerItemDecoration(Context context) {
+        mDivider = context.getResources().getDrawable(R.drawable.line_divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+}
